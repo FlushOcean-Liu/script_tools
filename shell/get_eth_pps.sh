75670d2d6a20e8f15cfd23fb0f2c69e5f2f38acd
liugh <1509444199@qq.com>
2022-07-20 17:03:01 +0800

    liugh:添加get_eth_pps.sh获取指定网卡收发报文的pps

---
+++ shell/get_eth_pps.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+sleep_time="1"
+
+if [ -z "$1" ]; then
+    echo
+    echo usage: $0 [network-interface]
+    echo
+    echo e.g. $0 eth0
+    echo
+    echo show eth device pps
+    exit
+fi
+
+dev=$1
+
+while true
+do
+    rx_start=`cat /sys/class/net/$dev/statistics/rx_packets`
+    tx_start=`cat /sys/class/net/$dev/statistics/tx_packets`
+
+    sleep $sleep_time
+
+    rx_end=`cat /sys/class/net/$1/statistics/rx_packets`
+    tx_end=`cat /sys/class/net/$1/statistics/tx_packets`
+
+    rx_pps=`expr $tx_end - $tx_start`
+    tx_pps=`expr $rx_end - $rx_end`
+
+    echo "tx $dev: $tx_pps pkts/s -- rx $dev: $rx_pps pkts/s"
+done
+
