7ec263a75961f9fc584e537f1a91e3389a427622
liugh <1509444199@qq.com>
2023-03-01 13:42:17 +0800

    修正get_eth_pps.sh错误

--- tools/get_eth_pps.sh
+++ tools/get_eth_pps.sh
@@ -21,11 +21,11 @@ do
 
     sleep $sleep_time
 
-    rx_end=`cat /sys/class/net/$1/statistics/rx_packets`
-    tx_end=`cat /sys/class/net/$1/statistics/tx_packets`
+    rx_end=`cat /sys/class/net/$dev/statistics/rx_packets`
+    tx_end=`cat /sys/class/net/$dev/statistics/tx_packets`
 
-    rx_pps=`expr $tx_end - $tx_start`
-    tx_pps=`expr $rx_end - $rx_end`
+    rx_pps=`expr $rx_end - $rx_start`
+    tx_pps=`expr $tx_end - $tx_start`
 
     echo "tx $dev: $tx_pps pkts/s -- rx $dev: $rx_pps pkts/s"
 done
